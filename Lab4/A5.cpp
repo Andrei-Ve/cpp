6bc1157cc5b756098ad3056df35d0bb608a7a638
Andrei-Ve <andrei.velikainen@gmail.com>
2025-04-20 23:16:23 +0300

    Finished A5, can be improved a little + comments missing

--- Lab4/A5.cpp
+++ Lab4/A5.cpp
@@ -9,7 +9,12 @@ class Car{
     public:
         Car(std::string m, int y, float p) : model(m), year(y), price(p) {}
 
-        ~Car(){}
+        ~Car(){
+            std::cout << "Destroyed " << model;
+        }
+
+        virtual void input(){}
+        virtual void display() const{}
 
         void inputBasic(std::string m, int y, float p){ 
             model = m;
@@ -18,7 +23,7 @@ class Car{
         }
 
         void displayBasic() const{
-            std::cout << "Model: " << model << ", Year: " << year << ", Price: " << price << std::endl;
+            std::cout << "Model: " << model << ", Year: " << year << ", Price: " << price;
         }
 };
 
@@ -26,12 +31,14 @@ class ElectricCar : public Car{
     private:
         float batterySize;
     public:
+        ElectricCar(std::string m, int y, float p) : Car(m, y, p){}
         void input(){
-
+            std::cout << "Enter battery size (kWh): ";
+            std::cin >> batterySize;
         }
 
         void display() const{
-        
+            std::cout << ", Batterysize: " << batterySize << std::endl;
         }
 
 };
@@ -40,12 +47,14 @@ class GasCar : public Car{
     private:
         float fuelCapacity;
     public:
+        GasCar(std::string m, int y, float p) : Car(m, y, p){}
         void input(){
-
+            std::cout << "Enter fuelc capacity (L): ";
+            std::cin >> fuelCapacity;
         }
 
         void display() const{
-
+            std::cout << ", Fuelcapacity: " << fuelCapacity << std::endl;
         }
 
 };
@@ -55,12 +64,79 @@ class HybridCar : public Car{
         float batterySize;
         float fuelCapacity;
     public:
+        HybridCar(std::string m, int y, float p) : Car(m, y, p){}
         void input(){
-
+            std::cout << "Enter battery size (kWh): ";
+            std::cin >> batterySize;
+            std::cout << "Enter fuelc capacity (L): ";
+            std::cin >> fuelCapacity;
         }
 
         void display() const{
-
+            std::cout << ", Batterysize: " << batterySize << ", Fuelcapacity: " << fuelCapacity << std::endl;
         }
 
 };
+
+int main(){
+    int numberOfCars;
+    std::cout << "Enter number of cars in the fleet: ";
+    std::cin >> numberOfCars;
+    std::cin.ignore();
+
+    Car** CarFleet = new Car*[numberOfCars];
+    int* CarType = new int[numberOfCars];
+
+    std::string model;
+    int year;
+    float price;
+
+    for(int i = 0; i < numberOfCars; ++i){
+        std::cout << "\nCar #" << i+1 << " type:\n[1] Electric\n[2] Gasoline\n[3] Hybrid" << std::endl;
+       
+        std::cout << "Enter choice: ";
+        std::cin >> CarType[i];
+        // std::cout << "    car type is   " << *FleetType[i];
+        std::cin.ignore();
+
+        std::cout << "Enter model: ";
+        std::getline(std::cin, model);
+        std::cout << "Enter year: ";
+        std::cin >> year;
+        std::cout << "Enter price: ";
+        std::cin >> price;
+
+        CarFleet[i] = new Car(model, year, price);
+
+        switch (CarType[i]) {
+            case 1:
+                CarFleet[i] = new ElectricCar(model, year, price);
+                break;
+            case 2:
+                CarFleet[i] = new GasCar(model, year, price);
+                break;
+            case 3:
+                CarFleet[i] = new HybridCar(model, year, price);
+                break;
+            default:
+                break;
+        }
+
+        CarFleet[i]->input();
+    }
+    std::cout << "\n~~~~Fleet Info~~~~\n";
+    for(int i = 0; i < numberOfCars; ++i){
+        CarFleet[i]->displayBasic();
+        CarFleet[i]->display();
+    }
+
+    std::cout << "\n~~~~Clearing fleet~~~~\n";
+    for (int i = 0; i < numberOfCars; ++i){
+        delete CarFleet[i];
+    }
+
+    delete[] CarFleet;
+    delete[] CarType;
+
+    return 0;
+}
